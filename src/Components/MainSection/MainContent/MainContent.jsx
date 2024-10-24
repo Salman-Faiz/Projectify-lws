558bc4ed8fc6e0db3ba1288849031857f4e11e62
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-10-24 13:43:51 +0600

    add tash modal added on toggle

--- src/Components/MainSection/MainContent/MainContent.jsx
+++ src/Components/MainSection/MainContent/MainContent.jsx
@@ -1,58 +1,50 @@
-import addTaskIcon from '../../../assets/Images/AddTaskIcon.svg'
+import { useState } from 'react'
 import DoneList from './Categories.jsx/DoneList'
 import OnProgress from './Categories.jsx/OnProgress'
 import ReviseList from './Categories.jsx/ReviseList'
 import TodoList from './Categories.jsx/TodoList'
+import addIcon from '../../../assets/Images/AddTaskIcon.svg'
+import AddTaskModal from '../../AddTaskModal/AddTaskModal'
 
 const MainContent = () => {
+  const [addTaskModal,setAddTaskModal] = useState(false);
+
+  const handleShowAddTask = () =>{
+    setAddTaskModal(true);
+  }
+  const handleCloseAddTask = () =>{
+    setAddTaskModal(false);
+  }
   return (
-    <div class="mx-auto max-w-7xl p-6">
-    <div class="mb-6 flex items-center justify-between">
-      <h2 class="text-2xl font-bold">Projectify</h2>
-      <div class="flex space-x-2">
-        <button
-          class="flex items-center rounded-md bg-gray-700 px-4 py-2 text-white"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="mr-2"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path
-              d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z"
-            />
-            <path d="M15 12h-6" />
-            <path d="M12 9v6" />
-          </svg>
-          Add
-        </button>
+    <>
+    {
+      addTaskModal && <AddTaskModal onClose={handleCloseAddTask}/>
+    }
+    <div className='mx-auto max-w-7xl p-6'>
+      <div className='mb-6 flex items-center justify-between'>
+        <h2 className='text-2xl font-bold'>Projectify</h2>
+        <div className='flex space-x-2'>
+          <a href="#" onClick={handleShowAddTask}>
+          <button className='flex items-center rounded-md bg-gray-700 px-4 py-2 text-white'>
+            <img src={addIcon} alt="" className='me-2'/>
+            Add
+          </button>
+          </a>
+        </div>
+      </div>
+      {/* categoris */}
+      <div className='-mx-2 mb-6 flex flex-wrap'>
+        <TodoList />
+
+        <OnProgress />
+
+        <DoneList />
+
+        <ReviseList />
       </div>
     </div>
-
-    <div class="-mx-2 mb-6 flex flex-wrap">
-
-
-
-    <TodoList />
-
-   <OnProgress />
-
-      <DoneList />
-
-     <ReviseList />
-
-
-
-    </div>
-  </div>
+    </>
+    
   )
 }
 
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,4 +1,3 @@
-
 import logo from '../assets/Images/lws-logo-en.svg'
 import dashBoard from '../assets/Images/sidebarIcons/dashBoard.svg'
 import project from '../assets/Images/sidebarIcons/project.svg'
@@ -8,83 +7,66 @@ import messages from '../assets/Images/sidebarIcons/messages.svg'
 import calender from '../assets/Images/sidebarIcons/Calender.svg'
 import settings from '../assets/Images/sidebarIcons/setting.svg'
 const Sidebar = () => {
-    return (
-         <>
-            <aside className="hidden w-64 bg-gray-800 p-6 lg:block">
-        <div className="mb-8 flex items-center">
-          <div
-            className="flex items-center justify-center rounded-full text-xl font-bold"
-          >
-            <img
-              src={logo}
-              className="mx-auto h-10 text-center"
-            />
+  return (
+    <>
+      <aside className='hidden w-64 bg-gray-800 p-6 lg:block'>
+        <div className='mb-8 flex items-center'>
+          <div className='flex items-center justify-center rounded-full text-xl font-bold'>
+            <img src={logo} className='mx-auto h-10 text-center' />
           </div>
         </div>
-        <button className="mb-8 w-full rounded-md bg-green-500 py-2 text-white">
+        <button className='mb-8 w-full rounded-md bg-green-500 py-2 text-white'>
           + New Project
         </button>
         <nav>
-          <ul className="space-y-4">
+          <ul className='space-y-4'>
             <li>
-              <a href="#" className="flex items-center"
-                >
-                    <img className='w-6 me-2' src={dashBoard} alt="" />
-                Dashboard</a
-              >
+              <a href='#' className='flex items-center'>
+                <img className='w-6 me-2' src={dashBoard} alt='' />
+                Dashboard
+              </a>
             </li>
             <li>
-              <a href="#" className="flex items-center"
-                >
-                    <img className='w-6 me-2' src={project} alt="" />
-                
-                Projects</a
-              >
+              <a href='#' className='flex items-center'>
+                <img className='w-6 me-2' src={project} alt='' />
+                Projects
+              </a>
             </li>
             <li>
-              <a href="#" className="flex items-center"
-                >
-                    <img className='w-6 me-2' src={contact} alt="" />
-                
-                Contact</a
-              >
+              <a href='#' className='flex items-center'>
+                <img className='w-6 me-2' src={contact} alt='' />
+                Contact
+              </a>
             </li>
             <li>
-              <a href="#" className="flex items-center"
-                >
-                   <img className='w-6 me-2' src={kanban} alt="" />
-                Kanban</a
-              >
+              <a href='#' className='flex items-center'>
+                <img className='w-6 me-2' src={kanban} alt='' />
+                Kanban
+              </a>
             </li>
             <li>
-              <a href="#" className="flex items-center"
-                >
-                  <img className='w-6 me-2' src={calender} alt="" />
-                Calendar</a
-              >
+              <a href='#' className='flex items-center'>
+                <img className='w-6 me-2' src={calender} alt='' />
+                Calendar
+              </a>
             </li>
             <li>
-              <a href="#" className="flex items-center"
-                ><img className='w-6 me-2' src={messages} alt="" />
-               
-                Messages</a
-              >
+              <a href='#' className='flex items-center'>
+                <img className='w-6 me-2' src={messages} alt='' />
+                Messages
+              </a>
             </li>
             <li>
-              <a href="#" className="flex items-center"
-                >
-                    <img className='w-6 me-2' src={settings} alt="" />
-                  
-                Settings</a
-              >
+              <a href='#' className='flex items-center'>
+                <img className='w-6 me-2' src={settings} alt='' />
+                Settings
+              </a>
             </li>
           </ul>
         </nav>
-      </aside> 
-        </>
-    )  
-       
+      </aside>
+    </>
+  )
+}
 
-};
-
-export default Sidebar;
+export default Sidebar
